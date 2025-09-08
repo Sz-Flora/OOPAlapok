20564a5d92741d843a61e38551bebcd79d3297fe
SzFlora <szabof@kkszki.hu>
2025-09-08 11:53:12 +0200

    Feladat1 is ready to use.

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOPAlapok
 {
+    public class Szemely
+    {
+        public string nev = "Peti";
+        public int kor = 22;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely person = new Szemely();
+            Console.WriteLine($"A személy neve: {person.nev} és életkora: {person.kor}");
         }
     }
 }
