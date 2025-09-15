90608fb1a0298ab2142b786c8b9cf8a4456286a5
SzFlora <szabof@kkszki.hu>
2025-09-15 09:12:58 +0200

    Feladat4 is ready

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -9,28 +9,18 @@ namespace OOPAlapok
 {
     public class Szemely
     {
-<<<<<<< HEAD
+
         private string _nev;
         private int _kor;
 
         public string Nev
         {
             get { return _nev; } //private adattag kiolvasásához
-            set { _nev = value; } //private adattag beállításához
-=======
-        private string nev;
-        private int kor;
-
-        public Szemely(string Nev, int Kor)
-        {
-            nev = Nev;
-            kor = Kor;
->>>>>>> 5aacd71b631b7b820f135c7d80caad79ab73a233
+            set { _nev = value; } //private adattag beállításához   
         }
 
         public int Kor
         {
-<<<<<<< HEAD
             get { return _kor; }
             set 
             {
@@ -39,6 +29,11 @@ namespace OOPAlapok
                 else
                     Console.WriteLine("Az életkor nem lehet negatív.");
             }
+        }
+
+        public override string ToString()
+        {
+            return $"A személy neve:{_nev}, életkora:{_kor}";
         }
 
         /*public Szemely(string Nev, int Kor)
@@ -51,43 +46,21 @@ namespace OOPAlapok
         {
             return $"A személy neve: {nev} és életkora: {kor}";
         }*/
-=======
-            get { return kor; }
-            set
-            {
-                if (value < 0)
-                {
-                    Console.WriteLine("Hiba: Az életkor nem lehet negatív! 0-ra állítva.");
-                    kor = 0;
-                }
-                else
-                {
-                    kor = value;
-                }
-            }
-        } 
 
-        public string Kiir()
-        {
-            return $"A személy neve: {nev} és életkora: {kor}";
-        }
->>>>>>> 5aacd71b631b7b820f135c7d80caad79ab73a233
+          
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             Szemely person = new Szemely();
             person.Nev = "Kiss Ilona";
             person.Kor = 23;
-            Console.WriteLine($"A személy neve:{person.Nev} és {person.Kor} éves");
+            Console.WriteLine(person);
             /*Szemely person = new Szemely("Ilona", 44);
             Console.WriteLine(person.Kiir());*/
-=======
-            Szemely person = new Szemely("Ilona", 44);
-            Console.WriteLine(person.Kiir());
->>>>>>> 5aacd71b631b7b820f135c7d80caad79ab73a233
+            Console.WriteLine();
         }
     }
 }
